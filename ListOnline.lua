ef2ee028417cd7cbed05bd8a719dccbe23cc1679
atomic86447 <161014320+atomic86447@users.noreply.github.com>
2024-03-13 12:46:58 +0700

    Create ListOnline.lua

---
+++ ListOnline.lua
@@ -0,0 +1,87 @@
+BossGem = {
+ 
+'Gilgamesh',
+'Natsu',
+'Ichigo',
+'Sukuna',
+'Gojo',
+'Uraume',
+'Sukuna',
+'Kashimo',
+'Tatsumaki',
+'Artia',
+'Rimuru',
+'Shank',
+'Sand Man',
+'Bomb Man',
+'Snow Bandit Leader' 
+
+}
+
+OrbSekkBossOnline = {
+ 
+ "Infinity Orb",
+ "[Choso] Cursed Womb",
+ "Sukuna Fingers Bag",
+ "Heart Bag"
+ 
+}
+
+BossWorld = {
+ 
+ 'Gojo [Unleashed]',
+ 'Killua',
+ 'Sukuna [Half Power]',
+ 'Choso'
+ 
+}
+
+GateMob = {
+ 
+ 'Shadow Soldier',
+ 'Shadow Lord',
+ 'Cid',
+ 'Shadow Sukuna',
+ 'Shadow Artoria',
+ 'LieutenantMarine',
+ 'MarineDungeon'
+ 
+}
+
+
+MeleeName = {
+ 
+"Combat",
+"Sukuna",
+"Gojo",
+"Gilgamesh",
+"Gon",
+"Itadi",
+"Uraume",
+"Hakari",
+"Sukuna [Half Power]",
+"Killua",
+"Gojo [Unleashed]"
+
+}
+ 
+ SwordName {
+  
+"Saber",
+"Excalibur",
+"Cutlass",
+"Katana",
+"Ichigo's Sword",
+"Kashimo's Pole Curse",
+"Cid's Swd",
+"Kashimo's Pole",
+"Rimuru's Swd"
+
+ }
+ 
+FruitList = {
+ 
+"God Light Fruit",
+"Dark Flame Fruit"
+
+ }
